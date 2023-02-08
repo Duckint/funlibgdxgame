eea904b855165b164f4538bfe3b62fbb041b220d
Duckint <90068370+TPWAGC@users.noreply.github.com>
2023-02-08 06:59:44 -0500

    alright, now everything should work

---
+++ core/src/com/ducky/game/Shapes.java
@@ -0,0 +1,14 @@
+package com.ducky.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+public class Shapes {
+
+      public void rectangles(float speedX, float speedY, float x, float y )
+      {
+
+      }
+
+}
