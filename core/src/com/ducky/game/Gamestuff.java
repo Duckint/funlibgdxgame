c361496c0cb82d0f6e4e8aeb4260a0053651775c
Duckint <90068370+TPWAGC@users.noreply.github.com>
2023-02-14 18:43:12 -0500

    time
    i can also get when the player is jumping and if they can at all.

--- core/src/com/ducky/game/Gamestuff.java
+++ core/src/com/ducky/game/Gamestuff.java
@@ -7,11 +7,15 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Input.Keys;
 import static com.badlogic.gdx.Gdx.graphics;
 import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.utils.TimeUtils;
 
 public class Gamestuff extends ApplicationAdapter {
 
       float speedX = 60;
 	  float speedY = 60;
+	  long startTime;
+
+
 
 	  //int test1 = 50;
 	  //int test2 = 50;
@@ -19,11 +23,16 @@ public class Gamestuff extends ApplicationAdapter {
 
 
 	  Player objMPlayer = new Player();
-	  ShapeRenderer rects;
-	  Platforms rectangle1 = Platforms.createRectangle(100, 100, 100, 100);
 
-	  public void input()
+	  ShapeRenderer rects;
+	  Platforms rectangle1 = Platforms.createRectangle(100, 100, 200, 20);
+
+	
+
+	public void input()
 	 {
+
+
 		 if(Gdx.input.isKeyPressed(Keys.A))
 		 {
 			 objMPlayer.player.x -= speedX * graphics.getDeltaTime();
@@ -35,6 +44,7 @@ public class Gamestuff extends ApplicationAdapter {
 		 if(Gdx.input.isKeyPressed(Keys.W))
 		 {
 			 objMPlayer.player.y += speedY * graphics.getDeltaTime();
+			 objMPlayer.isPlayerJumping = true;
 		 }
 	 }
     @Override
@@ -43,19 +53,30 @@ public class Gamestuff extends ApplicationAdapter {
 
 	   objMPlayer.playerRender = new ShapeRenderer();
 	   rects = new ShapeRenderer();
+	   startTime = TimeUtils.nanoTime();
+
+
 
 
 	}
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 1, 1);
-
+		 float secondsElapsed = (TimeUtils.nanoTime() - startTime) / 1_000_000_000f;
 		 objMPlayer.playerdraw();
 		 input();
-		 if(Intersector.overlaps(objMPlayer.player, rectangle1.getRectangle()))
+		 if(objMPlayer.isPlayerJumping)
 		 {
-			 System.out.println("hey im colliding lmfao");
+            if(secondsElapsed >= 10)
+			{
+
+			}
 		 }
+
+
+		 //if(Intersector.overlaps(objMPlayer.player, rectangle1.getRectangle()))
+	     //System.out.println("hey im colliding lmfao")
+
 		 rects.begin(ShapeRenderer.ShapeType.Line);
 		 rects.rect(rectangle1.getRectangle().x, rectangle1.getRectangle().y, rectangle1.getRectangle().width, rectangle1.getRectangle().height) ;
          rects.end();
--- core/src/com/ducky/game/Player.java
+++ core/src/com/ducky/game/Player.java
@@ -4,8 +4,12 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class Player {
     ShapeRenderer playerRender;
-    public Rectangle player = new Rectangle(50, 100, 50, 50);
+    public Rectangle player = new Rectangle(100, 140, 50, 50);
     public float playerFallSpeed = 20;
+    public boolean isPlayerJumping;
+
+    //this is just stating that I want it to be true by default.
+    public boolean canJump = true;
 
     public void playerdraw()
     {
