aa553b26fc02580c86ce2d51528123239d71dda7
Jonathan Pendergast <music80001@gmail.com>
2023-04-26 12:49:54 -0400

    Update Quadtree.java
    AAAAAAAAAAAAAAAAAAAAA TRYING TO FIGURE THE SYNTAX AND HOW TO IMPLEMENT THIS IS SOOO HARD CAUSE ME DUMB

--- core/src/com/ducky/game/Quadtree.java
+++ core/src/com/ducky/game/Quadtree.java
@@ -1,4 +1,25 @@
 package com.ducky.game;
 
+import jdk.internal.org.objectweb.asm.tree.analysis.Value;
+
+import java.security.Key;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Quadtree {
+    class Node {
+        Key x, y;
+        Node NW, NE, SW, SE;
+        Value value;
+
+        Node(Key x, Key y, Value value) {
+            this.x = x;
+            this.y = y;
+            this.value = value;
+        }
+    }
+    public void insert()
+    {
+
+    }
 }
